407a69af9ff0f9ca7128a011016ae03188cfe572
tclindner <tclindner@users.noreply.github.com>
2019-07-13 14:21:41 -0500

    Add jest linting

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ module.exports = {
     variables,
     prettier
   ],
-  plugins: ['prettier'],
+  plugins: ['jest', 'prettier'],
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module'
@@ -40,6 +40,35 @@ module.exports = {
         'array-bracket-newline': 'off',
         'array-element-newline': 'off',
         'id-length': 'off',
+        'jest/no-alias-methods': 'error',
+        'jest/no-disabled-tests': 'error',
+        'jest/no-focused-tests': 'error',
+        'jest/no-identical-title': 'error',
+        'jest/no-jasmine-globals': 'error',
+        'jest/no-jest-import': 'error',
+        'jest/no-test-prefixes': 'error',
+        'jest/valid-describe': 'error',
+        'jest/valid-expect-in-promise': 'error',
+        'jest/valid-expect': 'error',
+        'jest/consistent-test-it': [
+          'error',
+          {
+            fn: 'test',
+            withinDescribe: 'test'
+          }
+        ],
+        'jest/no-commented-out-tests': 'error',
+        'jest/no-empty-title': 'error',
+        'jest/no-test-callback': 'error',
+        'jest/no-test-return-statement': 'error',
+        'jest/no-truthy-falsy': 'error',
+        'jest/prefer-strict-equal': 'error',
+        'jest/prefer-to-be-null': 'error',
+        'jest/prefer-to-be-undefined': 'error',
+        'jest/prefer-to-contain': 'error',
+        'jest/prefer-to-have-length': 'error',
+        'jest/require-tothrow-message': 'error',
+        'jest/prefer-todo': 'error',
         'max-lines': 'off',
         'max-lines-per-function': 'off',
         'max-nested-callbacks': 'off',
