7d2086e1d426dcfadaa5e0dd123d8fc2a60f399e
Phil <48596606+phil-rw@users.noreply.github.com>
2019-12-05 09:54:56 -0600

    jest additions (#105)
    Adds agreed upon Jest additions:
    * closes #90 - jest/expect-expect
    * closes #89 - jest/prefer-called-with
    * closes #87 - jest/prefer-hooks-on-top
    * closes #85 - jest/require-top-level-describe

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ module.exports = {
             withinDescribe: 'test'
           }
         ],
+        'jest/expect-expect': 'error',
         'jest/no-alias-methods': 'error',
         'jest/no-commented-out-tests': 'error',
         'jest/no-disabled-tests': 'error',
@@ -66,6 +67,8 @@ module.exports = {
         'jest/no-test-return-statement': 'error',
         'jest/no-truthy-falsy': 'error',
         'jest/no-try-expect': 'error',
+        'jest/prefer-called-with': 'error',
+        'jest/prefer-hooks-on-top': 'error',
         'jest/prefer-strict-equal': 'error',
         'jest/prefer-to-be-null': 'error',
         'jest/prefer-to-be-undefined': 'error',
@@ -73,6 +76,7 @@ module.exports = {
         'jest/prefer-to-have-length': 'error',
         'jest/prefer-todo': 'error',
         'jest/require-to-throw-message': 'error',
+        'jest/require-top-level-describe': 'error',
         'jest/valid-describe': 'error',
         'jest/valid-expect-in-promise': 'error',
         'jest/valid-expect': 'error',
