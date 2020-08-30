b4ecad2c2f8b55cf0524acf5f318237a66ad8c80
Phil Ohnemus <1946398+pohnemus@users.noreply.github.com>
2020-08-30 13:36:18 -0500

    no-unreachable-loop

--- rules/best-practices.js
+++ rules/best-practices.js
@@ -15,6 +15,7 @@ module.exports = {
     'no-unmodified-loop-condition': 'error',
     'no-unused-expressions': 'error',
     'no-useless-call': 'error',
+    'no-unreachable-loop': 'error',
     'no-warning-comments': 'warn',
     'require-await': 'error',
   },
