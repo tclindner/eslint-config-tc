9f33c1b5bddff31918d654324c775a04d5a5fd1a
Phil Ohnemus <1946398+pohnemus@users.noreply.github.com>
2020-12-06 11:37:49 -0600

    Remove `import/no-extraneous-dependencies` overrides and just use Airbnb

--- rules/import.js
+++ rules/import.js
@@ -1,12 +1,5 @@
 module.exports = {
   rules: {
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        devDependencies: ['**/*.spec.js', '**/*.test.js', '**/tests-*.js', '**/*.spec.ts', '**/*.test.ts', '**/tests-*.ts'],
-        peerDependencies: false,
-      },
-    ],
     'import/prefer-default-export': 'off',
   },
 };
