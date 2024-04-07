1c93addfbcc5d14e8e723331c67fdf0e94dc98be
Thomas Lindner <5178550+tclindner@users.noreply.github.com>
2024-04-07 18:02:30 +0000

    Remove jest/no-if

--- rules/jest.js
+++ rules/jest.js
@@ -9,7 +9,6 @@ module.exports = {
     ],
     'jest/no-alias-methods': 'error',
     'jest/no-duplicate-hooks': 'error',
-    'jest/no-if': 'error',
     'jest/no-large-snapshots': 'error',
     'jest/no-test-return-statement': 'error',
     'jest/prefer-called-with': 'error',
